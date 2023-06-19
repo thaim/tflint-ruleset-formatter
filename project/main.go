b2a12e2fc8a5040b35d56ad2886f6f0100bcf228
thaim <thaim24@gmail.com>
2023-06-19 21:16:01 +0900

    add project to generate reference link

---
+++ project/main.go
@@ -0,0 +1,20 @@
+package project
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// ReferenceLink returns the rule reference link
+func ReferenceLink(name string) string {
+	return fmt.Sprintf("https://github.com/thaim/tflint-ruleset-formatter/blob/%s/docs/rules/%s.md", getVersion(), name)
+}
+
+func getVersion() string {
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "main"
+	}
+
+	return i.Main.Version
+}
